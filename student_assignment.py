69126b677aba835ce57ec78d7c75099ea983ae19
q098756 <q098756@gmail.com>
2025-01-20 14:05:36 +0800

    Implement generate_hw01 v1

--- student_assignment.py
+++ student_assignment.py
@@ -5,12 +5,51 @@ from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage
+from langchain.prompts import ChatPromptTemplate
+from langchain.output_parsers import (ResponseSchema, StructuredOutputParser)
+from langchain.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate
 
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
+llm = AzureChatOpenAI(
+    model=gpt_config['model_name'],
+    deployment_name=gpt_config['deployment_name'],
+    openai_api_key=gpt_config['api_key'],
+    openai_api_version=gpt_config['api_version'],
+    azure_endpoint=gpt_config['api_base'],
+    temperature=gpt_config['temperature']
+)
 
 def generate_hw01(question):
-    pass
+    examples = [
+        {"input": "2024年台灣10月紀念日有哪些?", "output": '{\
+            "Result": [\
+                {\
+                    "date": "2024-10-10",\
+                    "name": "國慶日"\
+                }\
+            ]\
+        }'}
+    ]
+    example_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("human", "{input}"),
+            ("ai", "{output}"),
+        ]
+    )
+    few_shot_prompt = FewShotChatMessagePromptTemplate(
+        example_prompt=example_prompt,
+        examples=examples
+    )
+    final_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "你是一個使用台灣語言且會遵照範例JSON格式輸出答案的紀念日專家"),
+            few_shot_prompt,
+            ("human", "{input}"),
+        ]
+    )
+    response = llm.invoke(final_prompt.format(input="2024年台灣10月紀念日有哪些?")).content
+    return response
     
 def generate_hw02(question):
     pass
@@ -38,3 +77,7 @@ def demo(question):
     response = llm.invoke([message])
     
     return response
+
+question = "2024年台灣3月紀念日有哪些?"
+answer = generate_hw01(question)
+print(answer)
